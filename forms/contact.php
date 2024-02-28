7d10215172e9e5b122c6d8ff8850688f55a98b76
Priyanshu Sunil Patil <priyanshupatil1091@gmail.com>
2024-02-28 13:49:06 +0530

    rename files

--- forms/contact.php
+++ forms/contact.php
@@ -1,1 +1,22 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
+echo $name;
+// database connection
+
+$conn = new mysqli('localhost', 'root', '', 'contact');
+if ($conn->connect_error) {
+    die('connection failed : ' . $conn->connect_error);
+} else
+    $stmt = $conn->prepare("insert into `contact info`(name,email,subject,message)
+    values(?, ?, ?, ?)");
+$stmt->bind_param("ssss", $name, $email, $subject, $message);
+$stmt->execute();
+echo "form submitted";
+$stmt->close();
+$conn->close();
+
+?>
